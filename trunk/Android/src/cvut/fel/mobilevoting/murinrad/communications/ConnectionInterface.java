c86ae3754352ce9b7e03f3f02acf1eabae29a2d8
radqooo@gmail.com <radqooo@gmail.com@1b072592-256c-f4d7-0123-7317e455594d>
2011-04-14 22:40:58 +0000

    Refactored HTTPConnection Class, now more clear and using proper libraries

--- trunk/Android/src/cvut/fel/mobilevoting/murinrad/communications/ConnectionInterface.java
+++ trunk/Android/src/cvut/fel/mobilevoting/murinrad/communications/ConnectionInterface.java
@@ -2,13 +2,16 @@ package cvut.fel.mobilevoting.murinrad.communications;
 
 import java.util.ArrayList;
 
+import org.apache.http.message.BasicHeader;
+
 import cvut.fel.mobilevoting.murinrad.datacontainers.QuestionData;
 
 public interface ConnectionInterface {
 
 	public abstract void run();
 
-	public abstract void sendReq() throws Exception;
+	public abstract void postAndRecieve(String method, String URL,
+			ArrayList<BasicHeader> headers, String body);
 
 	public abstract void closeConnection();
 
---
+++ trunk/Android/src/cvut/fel/mobilevoting/murinrad/communications/CustomClient.java
@@ -0,0 +1,15 @@
+package cvut.fel.mobilevoting.murinrad.communications;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+
+
+
+public class CustomClient extends DefaultHttpClient {
+	public CustomClient() {
+		super();
+	}
+	
+	
+	
+
+}
