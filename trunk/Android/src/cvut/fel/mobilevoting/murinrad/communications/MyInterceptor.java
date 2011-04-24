ddfda3a62f488f770e8ef4ba21169d71ea07e801
radqooo@gmail.com <radqooo@gmail.com@1b072592-256c-f4d7-0123-7317e455594d>
2011-04-24 23:13:59 +0000

    minor GUI changes, repaired slow down bug

--- trunk/Android/src/cvut/fel/mobilevoting/murinrad/communications/MyInterceptor.java
+++ trunk/Android/src/cvut/fel/mobilevoting/murinrad/communications/MyInterceptor.java
@@ -16,41 +16,47 @@ import android.util.Log;
 
 public class MyInterceptor implements HttpResponseInterceptor {
 	ConnectionInterface server;
-public MyInterceptor(ConnectionInterface ci) {
-	super();
-	server = ci;
-	
-}
+
+	public MyInterceptor(ConnectionInterface ci) {
+		super();
+		server = ci;
+
+	}
+
 	@Override
 	public void process(HttpResponse response, HttpContext context)
 			throws HttpException, IOException {
-		server.parseResponceCode(response.getStatusLine().getStatusCode() + "");
-		Log.d("Android mobiel voting", "IM INT THE INTERCEPTOR");
-		Header[] cl = response.getHeaders("Content-length");
-		Log.d("Android mobiel voting", "content length");
-		if (cl != null && !cl[0].getValue().equals("0")) {
-			Log.d("Android mobile voting", "Goodies!");
-			HttpEntity hE = response.getEntity();
-			Log.d("Android mobile voting", "Whats innit?");
-			byte[] buffer = new byte[Integer.parseInt(cl[0].getValue())];
-			Log.d("Android mobile voting", "Delicious cake?");
-			hE.getContent().read(buffer);
-			Log.d("Android mobile voting", "You shouldnt have + " + new String(buffer) );
-			
-			try {
-				XMLParser.XMLParser.parseServerXML(new String(buffer)	,server.getParent(),(ConnectionHTTP) server);
-			} catch (SAXException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ParserConfigurationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		if (server.parseResponceCode(response.getStatusLine().getStatusCode()
+				+ "")) {
+			Log.d("Android mobiel voting", "IM INT THE INTERCEPTOR");
+			Header[] cl = response.getHeaders("Content-length");
+			Log.d("Android mobiel voting", "content length");
+			if (cl != null && !cl[0].getValue().equals("0")) {
+				Log.d("Android mobile voting", "Goodies!");
+				HttpEntity hE = response.getEntity();
+				Log.d("Android mobile voting", "Whats innit?");
+				byte[] buffer = new byte[Integer.parseInt(cl[0].getValue())];
+				Log.d("Android mobile voting", "Delicious cake?");
+				hE.getContent().read(buffer);
+				Log.d("Android mobile voting", "You shouldnt have + "
+						+ new String(buffer));
+
+				try {
+					XMLParser.XMLParser.parseServerXML(new String(buffer),
+							server.getParent(), (ConnectionHTTP) server);
+				} catch (SAXException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ParserConfigurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				// connection.receiveResponseEntity(new Basi)
+
 			}
 
-			// connection.receiveResponseEntity(new Basi)
-
 		}
-
 	}
 
 }
